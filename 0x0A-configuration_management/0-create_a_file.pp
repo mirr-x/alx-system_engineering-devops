0e6d6071de30dcffee1faa723b729c0f1a799bd5
amir <amir.exe@icloud.com>
2024-02-23 23:14:47 +0100

    of this

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# 0-create_a_file.pp in /tmp
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
